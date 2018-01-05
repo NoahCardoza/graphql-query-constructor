767a89844289c2e0e14a6dde600240671a09805b
Noah Cardoza <noahcardoza@gmail.com>
2018-01-05 11:11:17 -0800

    Tests: Passing

--- test.js
+++ test.js
@@ -1,29 +1,38 @@
+// Don't mind this file. It is for testing purposes only and only work locally.
+
 const GraphQLClient = require('./index');
 const fetch = require('node-fetch');
 
-const sendQuery = (graphQLParams) =>
+const debug = data => {
+  console.log(data)
+  return data
+}
+
+const prettify = obj => console.log(JSON.stringify(obj, null, 2)) // Pretty Print
+const call = fn => obj => obj[fn]()
+const get = key => obj => obj[key]
+
+const sendQuery = (graphQLData) =>
   fetch('http://localhost:8081/graphql', {
     method: 'post',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(graphQLParams)
-  }).then(res => res.json())
+    body: debug(JSON.stringify(graphQLData))
+  })
+  .then(call('json'))
+  .then(get('data'))
 
 const { Query, Mutation } = GraphQLClient(sendQuery)
 
-Query().field('songs', {search: 'Mod'}, [
-  'id',
-  'title',
-  Query()
-    .field('bookmarks', {}, [
-      'userId',
-    ])
-  ]
-)
-.send()
-.then(json =>
-  JSON.stringify(json, null, 2)
-)
-.then(console.log)
+const fetchSongs = args =>
+  Query('songs', args || {}, [
+    'title',
+    'artist',
+    'album'
+  ]).send()
+
+fetchSongs({
+  search: 'Mod'
+}).then(prettify)
